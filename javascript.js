b252c030e07b2d9ce0e21fe347704b0318cec015
Frontenddevkan <frontenddevkan@gmail.com>
2023-02-19 03:24:44 +0500

    init: redesign project

---
+++ javascript.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+
+
+
+
+
+
+
+
+
+
+// let date = new Date("2023-02-14");
+// alert(date);
+
+
+// let now = new Date();
+// alert( now );
+
+// let age = prompt('How old are you? Сколько тебе лет?', '');
+// let mеssage = (age <= 15) ? 'Whazzup!\n\nЧо как?!' :
+//    (age <= 39) ? 'Hi!\n\nПривет!' :
+//    'Hello!\n\nЗдравствуйте!';
+//  alert( message );
+
+//do some tests about knowlege
+
+
